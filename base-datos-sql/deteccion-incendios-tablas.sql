75be96ad8cc48a2116e8bc149eda1c9a1377addf
FranklinPerez <franklin9perez952012@gmail.com>
2023-10-25 10:25:39 -0600

    tablas DB sql

--- base-datos-sql/deteccion-incendios-tablas.sql
+++ base-datos-sql/deteccion-incendios-tablas.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 20, 2023 at 07:26 PM
+-- Generation Time: Oct 25, 2023 at 06:24 PM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.0.18
 
@@ -14,6 +14,26 @@ SET time_zone = "+00:00";
 --
 -- Database: `arc_deteccion_incendios`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `correo_notificar`
+--
+
+CREATE TABLE `correo_notificar` (
+  `id_correo` int(11) NOT NULL,
+  `correo_notificar` varchar(100) NOT NULL,
+  `codigo_verificacion` int(11) NOT NULL,
+  `estado_verificacion` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `correo_notificar`
+--
+
+INSERT INTO `correo_notificar` (`id_correo`, `correo_notificar`, `codigo_verificacion`, `estado_verificacion`) VALUES
+(2, 'franklin9perez952012@gmail.com', 691211, 'verificado');
 
 -- --------------------------------------------------------
 
@@ -34,12 +54,18 @@ CREATE TABLE `gas_fuego_detectado` (
 --
 
 INSERT INTO `gas_fuego_detectado` (`id_alarma`, `descripcion_alarma`, `tipo_alarma`, `fecha_alarma`, `hora_alarma`) VALUES
-(11, 'fuego detectado', 'fuego', '2023-10-20', '11:25:34'),
-(12, 'fuga de gas detectada', 'gas', '2023-10-20', '11:25:43');
+(15, 'fuga de gas detectada', 'gas', '2023-10-20', '13:03:24'),
+(16, 'fuego detectado', 'fuego', '2023-10-20', '13:03:52');
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `correo_notificar`
+--
+ALTER TABLE `correo_notificar`
+  ADD PRIMARY KEY (`id_correo`);
 
 --
 -- Indexes for table `gas_fuego_detectado`
@@ -52,8 +78,14 @@ ALTER TABLE `gas_fuego_detectado`
 --
 
 --
+-- AUTO_INCREMENT for table `correo_notificar`
+--
+ALTER TABLE `correo_notificar`
+  MODIFY `id_correo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `gas_fuego_detectado`
 --
 ALTER TABLE `gas_fuego_detectado`
-  MODIFY `id_alarma` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `id_alarma` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 COMMIT;
